ee3ac78654bdb4e1d672b153f0f730bf01d39740
Aksels Ledins <aksels.ledins@gmail.com>
2017-12-09 23:44:37 +0100

    (c) SafetySheet

---
+++ src/components/SafetySheet.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container main">
+    <div class="row">
+      <div class="twelve columns headline">
+        <h2>Safety Sheet</h2>
+      </div>
+    </div>
+    <div class="row">
+      <div class="twelve columns download-container">
+        <a href="/static/safetysheets/spray-itc-venom-eng.pdf" target="_blank">
+          <button>
+            download
+          </button>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import openInNewTab from '@/helpers/openInNewTab';
+
+export default {
+  name: 'SafetySheet',
+  methods: {
+    openInNewTab,
+  }
+};
+</script>
+
+<style scoped>
+
+img {
+  width: 100%;
+  height: 50px;
+}
+
+.headline {
+  padding-top: 7rem;
+  padding-bottom: 2rem;
+  text-align: center;
+}
+
+.row {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  -webkit-box-pack: justify;
+  -webkit-bow-align: center;
+
+  height: 100%;
+}
+
+h5 {
+  text-align: center;
+}
+
+.download-container {
+  text-align: center;
+}
+
+</style>
