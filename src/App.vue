2794d226c40ae7edd322e733b7906ae64d1f0582
Aksels Ledins <aksels.ledins@gmail.com>
2017-12-09 22:42:58 +0100

    (app.vue) force fonts

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -16,8 +15,25 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+* {
+  font-family: 'Open Sans', sans-serif !important;
+}
+
+h1, h2, h3, h4, h5, h6 {
+  font-family: 'Poiret One', cursive !important;
+}
+
+a {
+  color: #70798C;
+  text-decoration: none;
+}
+
+.row {
+  margin-left: -15px;
+  margin-right: -15px;
+}
+
 </style>
