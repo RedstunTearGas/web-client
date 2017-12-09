f7535ef1791f9ff82c0fffdac3f189161b3c829e
Aksels Ledins <aksels.ledins@gmail.com>
2017-12-09 22:42:39 +0100

    (c) MainPresentation

---
+++ src/components/MainPresentation.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container main">
+    <div class="row">
+      <div class="six columns">
+        <h1>ITC VENOM</h1>
+        <div>
+          <p>Most powerful &amp; convenient tear gas in the world.</p>
+          <p>100% Made in France</p>
+        </div>
+      </div>
+      <div class="six columns">
+        <img
+          src="../assets/illustration.png"
+          alt="redstun tear gas"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainPresentation',
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.container {
+ height: 100%;
+}
+
+
+
+.row {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  -webkit-box-pack: justify;
+  -webkit-bow-align: center;
+
+  height: 100%;
+}
+
+img {
+  display: block;
+  margin: 0 auto;
+}
+
+</style>
