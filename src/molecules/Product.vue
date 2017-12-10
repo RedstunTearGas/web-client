f8ef16ff5001bed78e16828c702de2d17038b0ad
Aksels Ledins <aksels.ledins@gmail.com>
2017-12-10 16:43:21 +0100

    (molecules) Product

---
+++ src/molecules/Product.vue
@@ -0,0 +1,61 @@
+<template>
+  <div :class="`${columnSize} columns product-container`">
+    <div class="product">
+      <br />
+      <h2 class="product-name">
+        {{title}}
+      </h2>
+      <img
+        :src="image"
+        alt="product-icon"
+      />
+      <div v-for="description in desc">
+        <small>{{description}}</small>
+      </div>
+      <br />
+      <div class="button-container">
+        <button>Price on demand</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Product',
+  props: ['title', 'desc', 'image', 'columnSize'],
+};
+</script>
+
+<style scoped>
+
+.product-container {
+  padding-top: 15px;
+  padding-bottom: 15px;
+}
+
+.product {
+  background-color: #FFF;
+  border: 1px solid #70798C;
+
+  text-align: center;
+
+  min-height: 39rem;
+
+  position: relative;
+}
+
+.button-container {
+  position: absolute;
+  bottom: 1rem;
+  left: 0;
+  right: 0;
+}
+
+img {
+  width: 30%;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+}
+
+</style>
