0b5adc9173cd6cb8bc7c752a980efd0be772e03a
Aksels Ledins <aksels.ledins@gmail.com>
2017-12-10 13:48:17 +0100

    (p) Additives

---
+++ src/pages/Additives.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <top-navigation />
+
+    <div class="container">
+      <div class="row">
+        <div class="twelve columns">
+          <h1>Raw Materials</h1>
+        </div>
+      </div>
+      <div class="row">
+        <a-product
+          title="ITC Additive"
+          :desc="
+            ['aimed to be mixed at a rate of 5% in water', 'for Police water-cannons only']
+          "
+          image="src/assets/icons/broccoli.svg"
+          columnSize="six"
+        />
+        <a-product
+          title="CS Additive"
+          :desc="
+            ['aimed to be mixed at a rate of 5% in water', 'for Police water-cannons only']
+          "
+          image="src/assets/icons/chili-pepper.svg"
+          columnSize="six"
+        />
+      </div>
+    </div>
+    <bottom-footer />
+  </div>
+</template>
+
+<script>
+
+import Product from '@/molecules/Product';
+
+export default {
+  name: 'Additives',
+  components: {
+    'a-product': Product,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+  .container {
+    margin-top: 10rem;
+    margin-bottom: 10rem;
+  }
+
+</style>
