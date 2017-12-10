2f7fed8ce62996b149b955a9f723a220b52d58ef
Aksels Ledins <aksels.ledins@gmail.com>
2017-12-10 19:26:16 +0100

    (main.js) +vue-analytics

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 
+import VueAnalytics from 'vue-analytics';
+
 import NavLink from '@/atoms/NavLink';
 import Navigation from '@/shared/Navigation';
 import Footer from '@/shared/Footer';
@@ -15,6 +17,10 @@ Vue.component('nav-link', NavLink);
 Vue.component('top-navigation', Navigation);
 Vue.component('bottom-footer', Footer);
 
+Vue.use(VueAnalytics, {
+  id: 'UA-110928879-1',
+  router,
+});
 
 /* eslint-disable no-new */
 new Vue({
