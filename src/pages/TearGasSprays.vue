1bebeb0e2544e448d8ba0699f35d923c983731d8
Aksels Ledins <aksels.ledins@gmail.com>
2017-12-10 15:11:36 +0100

    (p) TearGasSprays

---
+++ src/pages/TearGasSprays.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <top-navigation />
+
+    <div class="container">
+      <div class="row">
+        <div class="twelve columns">
+          <h1>Tear Gas Sprays</h1>
+        </div>
+      </div>
+      <h2>ITC Venom</h2>
+      <div class="row">
+        <a-product
+          title="25ml"
+          image="/static/sprays/itc-venom/front_25ml.jpg"
+          columnSize="four"
+        />
+        <a-product
+          title="50ml"
+          image="/static/sprays/itc-venom/front_50ml.jpg"
+          columnSize="four"
+        />
+        <a-product
+          title="75ml"
+          image="/static/sprays/itc-venom/front_100ml.jpg"
+          columnSize="four"
+        />
+      </div>
+      <br /><br />
+      <h2>OC Viper</h2>
+      <div class="row">
+        <a-product
+          title="25ml"
+          image="/static/sprays/oc-viper/front_25ml.jpg"
+          columnSize="six"
+        />
+        <a-product
+          title="50ml"
+          image="/static/sprays/oc-viper/front_50ml.jpg"
+          columnSize="six"
+        />
+      </div>
+    </div>
+    <bottom-footer />
+  </div>
+</template>
+
+<script>
+
+import Product from '@/molecules/Product';
+
+export default {
+  name: 'RawMaterials',
+  components: {
+    'a-product': Product,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+  .container {
+    margin-top: 10rem;
+    margin-bottom: 10rem;
+  }
+
+</style>
