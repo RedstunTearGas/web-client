85d68ce338dff3d401d0527e798e112dba11b9f6
Aksels Ledins <aksels.ledins@gmail.com>
2017-12-10 12:06:12 +0100

    (router) +Legal /legal

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+
 import Home from '@/pages/Home';
+import Legal from '@/pages/Legal';
 
 Vue.use(Router);
 
@@ -10,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home,
+    },
+    {
+      path: '/legal',
+      name: 'Legal',
+      component: Legal,
     },
   ],
 });
