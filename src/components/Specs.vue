3c8b0224be6c15da7e29bca927f86dd889d25204
Aksels Ledins <aksels.ledins@gmail.com>
2017-12-14 19:21:47 +0100

    (c) Specs : UX

--- src/components/Specs.vue
+++ src/components/Specs.vue
@@ -13,54 +13,76 @@
             alt="fireproof-icon"
           />
         </div>
-        <h5>Non-flammable</h5>
-        <p>
-          Fire-proof.
-        </p>
+        <div>
+          <h5>Non-flammable</h5>
+          <p>
+            Fire-proof.
+          </p>
+        </div>
       </div>
       <div class="six columns">
-        <img
-          src="../assets/icons/anise.svg"
-          alt="anise-icon"
-        />
-        <h5>Pleasent odor</h5>
-        <p>Smells Good. Aniseed.</p>
-      </div>
-    </div>
-    <div class="row">
-      <div class="six columns">
-        <img
-          src="../assets/icons/subway.svg"
-          alt="subway-icon"
-        />
-        <h5>Indoor</h5>
-        <p>Only the target will be stunned.</p>
-      </div>
-      <div class="six columns">
-        <img
-          src="../assets/icons/washing-machine.svg"
-          alt="washing-machine-icon"
-        />
-        <h5>Washable</h5>
-        <p>You can wash it away easily from clothes.</p>
+        <div>
+          <img
+            src="../assets/icons/anise.svg"
+            alt="anise-icon"
+          />
+        </div>
+        <div>
+          <h5>Pleasent odor</h5>
+          <p>Smells Good. Aniseed.</p>
+        </div>
       </div>
     </div>
     <div class="row">
       <div class="six columns">
-        <img
-          src="../assets/icons/chili-pepper.svg"
-          alt="chili-pepper-icon"
-        />
-        <h5>Strong</h5>
-        <p>5.5 millions SHU. Powerful.</p>
+        <div>
+          <img
+            src="../assets/icons/subway.svg"
+            alt="subway-icon"
+          />
+        </div>
+        <div>
+          <h5>Indoor</h5>
+          <p>Only the target will be stunned.</p>
+        </div>
       </div>
       <div class="six columns">
-        <img
-          src="../assets/icons/france.svg"
-          alt="france-icon"
-        />
-        <h5>Made in France</h5>
-        <p>Top. Notch. Quality.</p>
+        <div>
+          <img
+            src="../assets/icons/washing-machine.svg"
+            alt="washing-machine-icon"
+          />
+        </div>
+        <div>
+          <h5>Washable</h5>
+          <p>You can wash it away easily from clothes.</p>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="six columns">
+        <div>
+          <img
+            src="../assets/icons/chili-pepper.svg"
+            alt="chili-pepper-icon"
+          />
+        </div>
+        <div>
+          <h5>Strong</h5>
+          <p>5.5 millions SHU. Powerful.</p>
+        </div>
+      </div>
+      <div class="six columns">
+        <div>
+          <img
+            src="../assets/icons/france.svg"
+            alt="france-icon"
+          />
+        </div>
+        <div>
+          <h5>Made in France</h5>
+          <p>Top. Notch. Quality.</p>
+        </div>
       </div>
     </div>
   </div>
@@ -96,7 +118,15 @@ img {
 }
 
 h5 {
-  text-align: center;
+  text-align: left;
+}
+
+.columns {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  -webkit-box-pack: justify;
+  -webkit-bow-align: center;
 }
 
 </style>
