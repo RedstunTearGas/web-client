28ccae80c06520a9d01c43677e38c3dbab4cbfd0
Aksels Ledins <aksels.ledins@gmail.com>
2017-12-27 11:40:39 +0100

    (p) RawMaterials wording

--- src/pages/RawMaterials.vue
+++ src/pages/RawMaterials.vue
@@ -14,29 +14,36 @@
           :desc="
             ['origin China', 'purity > 98%']
           "
-          image="/static/icons/china.svg"
+          image="/static/icons/cs.png"
           columnSize="four"
         />
         <a-product
           title="Pure Capsaicin"
           :desc="
-            ['origin India', 'purity > 98%']
+            [
+              'purity > 98%',
+              'origin India',
+            ]
           "
-          image="/static/icons/india.svg"
+          image="/static/icons/capsaicin.svg"
           columnSize="four"
         />
         <a-product
           title="OC Resin"
           :desc="
             [
-              'origin India',
               'various Major Capsaicinoid content available',
               'water soluble or solvent soluble',
+              'origin India',
             ]
           "
-          image="/static/icons/india.svg"
+          image="/static/icons/barrel.svg"
           columnSize="four"
         />
+      </div>
+      <div class="info">
+        <p>Redstun is the agent of the company Southern Spices for Europe</p>
+        <a href="http://www.southernspices.co.in">Link to website</a>
       </div>
     </div>
     <bottom-footer />
@@ -61,6 +68,16 @@ export default {
   .container {
     margin-top: 10rem;
     margin-bottom: 10rem;
+  }
+
+  .info {
+    border-left: 1px solid #70798C;
+    padding-left: 2rem;
+    margin-top: 3rem;
+  }
+
+  .info p {
+    margin: 0;
   }
 
 </style>
