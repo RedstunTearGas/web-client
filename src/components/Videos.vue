0dde0891c76d6b3b9f0117c444d8a37bfbd59298
Aksels Ledins <aksels.ledins@gmail.com>
2018-03-03 18:03:01 +0100

    (c) Videos : add Riga test on videos

--- src/components/Videos.vue
+++ src/components/Videos.vue
@@ -6,13 +6,17 @@
       </div>
     </div>
     <div class="row">
-      <div class="six columns">
+      <div class="four columns">
         <h3>Test in France</h3>
         <iframe width="100%" height="315" src="https://www.youtube.com/embed/pQXfhqRwfbw?rel=0" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
       </div>
-      <div class="six columns">
+      <div class="four columns">
         <h3>Test in Brazil</h3>
         <iframe width="100%" height="315" src="https://www.youtube.com/embed/2ggmCJXT87s?rel=0" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
+      </div>
+      <div class="four columns">
+        <h3>Test in Riga</h3>
+        <iframe width="100%" height="315" src="https://www.youtube.com/embed/G_4ZVvyt3-w?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
       </div>
     </div>
   </div>
