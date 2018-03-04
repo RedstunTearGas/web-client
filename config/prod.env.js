b26ff59af7dd029e2911d55ce85a1d440e3997bf
Aksels Ledins <aksels.ledins@gmail.com>
2018-03-04 22:16:42 +0100

    (config) prod add ga id

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  WEBSITE_LOCALE: `"${process.env.WEBSITE_LOCALE || 'en'}"`
+  WEBSITE_LOCALE: `"${process.env.WEBSITE_LOCALE || 'en'}"`,
+  GA_ID: `"${process.env.GA_ID || 'UA-110928879-1'}"`,
 }
