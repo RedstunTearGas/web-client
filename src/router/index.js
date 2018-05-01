df0f17798f8d761f004dfd20b73a9550b85680d7
Aksels Ledins <aksels.ledins@gmail.com>
2018-05-01 20:01:54 +0200

    (router) add dynamic title to certain routes

--- src/router/index.js
+++ src/router/index.js
@@ -32,16 +32,25 @@ export default new Router({
       path: '/raw-materials',
       name: 'RawMaterials',
       component: RawMaterials,
+      meta: {
+        title: process.env.WEBSITE_LOCALE === 'fr' ? 'Redstun - Matières Premières' : 'Redstun - Raw Materials',
+      },
     },
     {
       path: '/additives',
       name: 'Additives',
       component: Additives,
+      meta: {
+        title: process.env.WEBSITE_LOCALE === 'fr' ? 'Redstun - Additifs pour canon à eau' : 'Redstun - Additives for Police water-cannons',
+      },
     },
     {
       path: '/tear-gas-sprays',
       name: 'TearGasSprays',
       component: TearGasSprays,
+      meta: {
+        title: process.env.WEBSITE_LOCALE === 'fr' ? 'Redstun - Bombes lacrymogènes de défense' : 'Redstun - Defense spray - Tear Gas',
+      },
     },
     {
       path: '/contact',
@@ -52,8 +61,10 @@ export default new Router({
       path: '/buy',
       name: 'Buy',
       component: Buy,
+      meta: {
+        title: process.env.WEBSITE_LOCALE === 'fr' ? 'Redstun - Où acheter' : 'Redstun - Where to buy',
+      },
     },
-
     {
       path: '/itc',
       name: 'ITC',
